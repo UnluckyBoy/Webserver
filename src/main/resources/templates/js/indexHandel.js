0bd57e1ceca38f3befb82f15d5b93da10b9403ae
UnluckyBoy <qfmy_zhang@163.com>
2024-05-09 22:03:56 +0800

    修正逻辑,添加UI功能 By Matrix

--- src/main/resources/templates/js/indexHandel.js
+++ src/main/resources/templates/js/indexHandel.js
@@ -2,20 +2,21 @@
  * index页面处理逻辑
  */
 // document.addEventListener('DOMContentLoaded', function() {
+var globalPatient;
 $(document).ready(function() {
-    sub_btn_view_Click();
+    //sub_btn_view_Click();
 
     // 挂号日期按钮获取时间逻辑
     $('#registration-time-btn').click(function() {
         // 获取当前日期和时间
         $('#registration-time').val(getCurrentTime());
     });
-});
 
-/**
- * 按钮响应逻辑
- */
-function sub_btn_view_Click(){
+    /**
+     * 按钮响应逻辑
+     */
+    // function sub_btn_view_Click(){
+    // }
     let $btn=$('#page1').children(0).children();
     $btn.each(function(i, btn) { //使用.each()遍历jQuery对象
         // 使用$(btn)将原生的DOM元素转换成jQuery对象
@@ -32,6 +33,7 @@ function sub_btn_view_Click(){
                     break;
                 case 'gh_save':
                     console.log('点击"保存"按钮', this.id);
+                    console.log("患者ID:"+globalPatient.patient_id);
                     break;
                 case 'gh_cancel':
                     console.log('点击"退号"按钮', this.id);
@@ -39,73 +41,65 @@ function sub_btn_view_Click(){
             }
         });
     });
-}
 
-/**
- * 获取组件
- */
-function clearPage1ViewElement(){
-    //console.log("info-view的子组件:",$('.info-view').children())
-    // $("#insurance-id").val('');
-    // $("#appointment-date").val('');
-    // $("#patient-name").val('');
-    // $('#patient-gender').prop('selectedIndex', 0).change(); //直接设置selectedIndex
-    // $('#patient-idCard').val('');
-    // $('#patient-idCard').val('');
-    showModal();
-    $('#read-idCard-btn').on('click',function (){
-        if($('#read-idCard-input').val().trim()!==''){
-            console.log("身份证号:",$('#read-idCard-input').val());
-            getPatientInfoHandle($('#read-idCard-input').val());
-            //hideModal();
-        }else{
-            alert("身份证为空");
-        }
-    });
-}
-function getPatientInfoHandle(patient){
-    $.ajax({
-        url:'/api/patientInfo',
-        type: 'POST',
-        data:{"patient":patient},
-        dataType: 'json',
-        success: function(data) {
-            if(data.handleType){
-                console.log(data);
-                hideModal();//隐藏弹窗
-                /*绑定信息*/
-                //$("#insurance-id").val(data.handleData.);
-                //$("#appointment-date").val();
-                $("#patient-name").val(data.handleData.patient_name);
-                $('#patient-gender').val(data.handleData.patient_gender);
-                $('#patient-idCard').val(data.handleData.patient_idCard);
-                $('#patient-birth').val(data.handleData.patient_birth);
-                $('#patient-nationality').val(data.handleData.patient_nationality);
-                $('#patient-nativePlace').val(data.handleData.patient_nativePlace);
-                $('#patient-nation').val(data.handleData.patient_nation);
-                $('#patient-occupation').val(data.handleData.patient_occupation);
-                $('#patient-maritalStatus').val(data.handleData.patient_maritalStatus);
-                $('#patient-phone').val(data.handleData.patient_phone);
-                $('#patient-age').val(data.handleData.patient_age);
-                $('#poverty-sign').val(data.handleData.poverty_sign);
-                $('#patient-emergencyContact').val(data.handleData.patient_emergencyContact);
-                $('#emergencyContact-relationship').val(data.handleData.patient_relationship);
-                $('#patient-contactPhone').val(data.handleData.patient_contactPhone);
-                $('#patient-homeAddress').val(data.handleData.patient_homeAddress);
-                $('#patient-workAddress').val(data.handleData.patient_workAddress);
-                $('#nowAddress-province').val(data.handleData.nowAddress_province);
-                $('#nowAddress-town').val(data.handleData.nowAddress_town);
-                $('#nowAddress-prefecture').val(data.handleData.nowAddress_prefecture);
-                if(data.handleData.child_sign==='是'){
-                    $('#child-sign').prop('checked')
-                }
-                $('#guardian-name').val(data.handleData.guardian_name);
-                $('#guardian-idCard').val(data.handleData.guardian_idCard);
-                $('#guardian-phone').val(data.handleData.guardian_phone);
+    /**
+     * 获取组件
+     */
+    function clearPage1ViewElement(){
+        showModal();
+        $('#read-idCard-btn').on('click',function (){
+            if($('#read-idCard-input').val().trim()!==''){
+                console.log("身份证号:",$('#read-idCard-input').val());
+                getPatientInfoHandle($('#read-idCard-input').val());
+                //hideModal();
+            }else{
+                alert("身份证为空");
             }
-        },
-        error: function(xhr, status, error) {
-            console.error("请求失败: " +error);
-        }
-    });
-}
+        });
+    }
+    function getPatientInfoHandle(patient){
+        $.ajax({
+            url:'/api/patientInfo',
+            type: 'POST',
+            data:{"patient":patient},
+            dataType: 'json',
+            success: function(data) {
+                if(data.handleType){
+                    console.log(data);
+                    hideModal();//隐藏弹窗
+                    globalPatient=data.handleData;
+                    /*绑定信息*/
+                    //$("#insurance-id").val(globalPatient.);
+                    //$("#appointment-date").val();
+                    $("#patient-name").val(globalPatient.patient_name);
+                    $('#patient-gender').val(globalPatient.patient_gender);
+                    $('#patient-idCard').val(globalPatient.patient_idCard);
+                    $('#patient-birth').val(globalPatient.patient_birth);
+                    $('#patient-nationality').val(globalPatient.patient_nationality);
+                    $('#patient-nativePlace').val(globalPatient.patient_nativePlace);
+                    $('#patient-nation').val(globalPatient.patient_nation);
+                    $('#patient-occupation').val(globalPatient.patient_occupation);
+                    $('#patient-maritalStatus').val(globalPatient.patient_maritalStatus);
+                    $('#patient-phone').val(globalPatient.patient_phone);
+                    $('#patient-age').val(globalPatient.patient_age);
+                    $('#poverty-sign').val(globalPatient.poverty_sign);
+                    $('#patient-emergencyContact').val(globalPatient.patient_emergencyContact);
+                    $('#emergencyContact-relationship').val(globalPatient.patient_relationship);
+                    $('#patient-contactPhone').val(globalPatient.patient_contactPhone);
+                    $('#patient-homeAddress').val(globalPatient.patient_homeAddress);
+                    $('#patient-workAddress').val(globalPatient.patient_workAddress);
+                    $('#nowAddress-province').val(globalPatient.nowAddress_province);
+                    $('#nowAddress-town').val(globalPatient.nowAddress_town);
+                    $('#nowAddress-prefecture').val(globalPatient.nowAddress_prefecture);
+                    $('#child-sign').val(globalPatient.child_sign);
+                    $('#guardian-name').val(globalPatient.guardian_name);
+                    $('#guardian-idCard').val(globalPatient.guardian_idCard);
+                    $('#guardian-phone').val(globalPatient.guardian_phone);
+                }
+            },
+            error: function(xhr, status, error) {
+                console.error("请求失败: " +error);
+            }
+        });
+    }
+});
